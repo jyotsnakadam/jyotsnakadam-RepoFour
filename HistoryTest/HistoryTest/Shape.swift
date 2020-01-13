d0b077a1a32cdb6a0afefbc53c3e38965b793bfd
Jyotsna Kadam <jyotsna.kadam@xoriant.net>
2020-01-13 12:04:28 +0530

    Added Xcode Project with Shape class

---
+++ HistoryTest/HistoryTest/Shape.swift
@@ -0,0 +1,21 @@
+//
+//  Shape.swift
+//  HistoryTest
+//
+//  Created by Jyotsna Kadam  on 13/01/20.
+//  Copyright © 2020 Jyotsna Kadam . All rights reserved.
+//
+
+import UIKit
+
+class Shape: NSObject {
+
+    //This function is written to calculate area of a shape
+    func calculateShapeArea()
+    {
+        let a = 10
+        let b = 20
+        let c = a * b
+        print(c)
+    }
+}
